9fd3270803d138fc056c01bf976a90e74d071165
Aamir Maniar <aamir.maniar@maniartech.com>
2020-06-04 22:47:25 +0530

    AST Parsign development started

--- internet_object/parsers/tokens.py
+++ internet_object/parsers/tokens.py
@@ -15,10 +15,25 @@ class Token:
     self.col = col
     self.type = token_type
     self.token = token  # text[start:end+1]
+    self.value = token
 
   def __str__(self):
     return "%s (%s, %s, %s, %s, %s)" % (
-        repr(self.token),
+        repr(self.value),
         self.type,
         self.start, self.end, self.row, self.col
     )
+
+  def __repr__(self):
+    return repr(self.to_dict())
+
+  def to_dict(self):
+    return {
+        # "start": self.start,
+        # "end": self.end,
+        # "row": self.row,
+        # "col": self.col,
+        "type": self.type,
+        # "token": self.token,
+        "value": self.value
+    }
