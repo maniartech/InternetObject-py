08b72fdc36d7cc1244499adc2a15b28474837629
Aamir Maniar <aamir.maniar@maniartech.com>
2020-06-05 23:22:34 +0530

    Worked on AST

--- internet_object/parsers/ast_parsers.py
+++ internet_object/parsers/ast_parsers.py
@@ -21,18 +21,20 @@ class AST:
 
   def parse(self):
     lexer = self.lexer
-    while True:
-      token = lexer.read()
+    self.tree["data"] = self.get_or_create_last_container()
 
-      if token is None:
-        continue
+    while True:
 
       if lexer.done:
         break
 
+      token = lexer.read()
+      if token is None:
+        continue
+
       self.process(token)
 
-    print("+++", self.stack)
+    print("+++", self.tree["data"])
 
   def get_object(self):
     pass
@@ -40,16 +42,19 @@ class AST:
   def process(self, token):
     if token.type == "sep":
       if token.value == "{":
-        self.push_node("object", token)
+        self.push_container("object", token)
 
       elif token.value == "}":
-        self.pop("object")
+        self.pop_container("object")
 
       elif token.value == "[":
-        self.push_node("collection", token)
+        self.push_container("array", token)
+
+      elif token.value == "]":
+        self.pop_container("array")
 
       elif token.value == "~":
-        self.push_node("collection", token)
+        self.push_container("collection", token)
 
       else:
         self.push_value(Node.from_token(token))
@@ -58,7 +63,6 @@ class AST:
       raise NotImplementedError("not-ready")
 
     else:
-
       self.push_value(Node.from_token(token))
 
   def push_value(self, value):
@@ -67,12 +71,12 @@ class AST:
     pipe = self.value_pipe
     pipe_len = len(pipe)
 
-    parent = self.get_or_create_last_node()
-    values = parent.value
+    container = self.get_or_create_last_container()
+    values = container.value
     values_len = len(values)
 
     try:
-      last_value = values[:1]
+      last_value = values[-1]
     except IndexError:
       last_value = None
 
@@ -84,11 +88,8 @@ class AST:
         values.append(undefined_node)
 
       # When value exists in the pipe clear it
-      elif pipe_len == 1:
+      elif pipe_len == 1 or pipe_len == 3:
         pipe.clear()
-
-      else:
-        assert False, "unexpected-block"
 
     elif is_collon:
       # Throw a syntax error
@@ -97,8 +98,8 @@ class AST:
           # When last value in the pipe is colon
           pipe[-1] == ":" or
               # When last value in the pipe is not a string
-              pipe[-1].type != "string"):
-
+              pipe[-1].type != "str"):
+        # print(pipe)
         raise SyntaxError("unexpected-colon")
 
       elif pipe_len == 1:
@@ -111,17 +112,26 @@ class AST:
         values.append(value)
 
       # When the last char in pipe is : setup key-value
-      if pipe_len == 2:
-        key = last_value
+      if pipe_len == 2 and pipe[-1] == ":":
+        # print("----")
+        # print(values)
+        # print(last_value)
+        last_value.key = last_value.value
+        last_value.value = value
+        pipe.append(value)
 
-  def push_node(self, object_type, token):
-    parent = self.get_last_node()
-    pos = parent["pos"] + 1 if parent is not None else 0
+  def pop_container(self, container_type):
+    container = self.stack[0]
+    self.stack = self.stack[1:]
+
+  def push_container(self, object_type, token):
+    parent = self.get_last_container()
+    pos = parent.pos + 1 if parent is not None else 0
     node = Node([], object_type, token.start,
                 token.end, token.row, token.col, pos, parent)
 
-    self.stack.append(node)
     self.push_value(node)
+    self.stack.append(node)
 
   def get_undefined_node(self, parent=None):
     index = 0 if parent is None else len(parent.value)
@@ -132,14 +142,14 @@ class AST:
     return Node(token.value, token.type, token.start,
                 token.end, token.raw, token.col, index, parent)
 
-  def get_last_node(self):
+  def get_last_container(self):
     if len(self.stack) == 0:
       return None
 
     return self.stack[-1]
 
-  def get_or_create_last_node(self):
-    node = self.get_last_node()
+  def get_or_create_last_container(self):
+    node = self.get_last_container()
     if node is None:
       node = Node([], "object", 0, 0, 1, 1, 0, None)
       self.stack.append(node)
