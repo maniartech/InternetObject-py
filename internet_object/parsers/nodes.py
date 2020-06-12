a6f82ff6cb93207c8ed1edf3eeb23100629957eb
Aamir Maniar <aamir.maniar@maniartech.com>
2020-06-12 10:54:30 +0530

    corrected Node's structure

--- internet_object/parsers/nodes.py
+++ internet_object/parsers/nodes.py
@@ -2,7 +2,7 @@
 
 class Node:
 
-  def __init__(self, value, node_type, start, end, row, col, pos=0, parent=None):
+  def __init__(self, val, node_type, start, end, row, col, pos=0, parent=None):
 
     self.start = start
     self.end = end
@@ -11,13 +11,13 @@ class Node:
 
     self.pos = 0
     self.key = None
-    self.value = value
+    self.val = val
     self.type = node_type
 
   @classmethod
   def from_token(cls, token, pos=0, parent=None):
     return Node(
-        token.value,
+        token.val,
         token.type,
         token.start,
         token.end,
@@ -26,20 +26,20 @@ class Node:
     )
 
   def __str__(self):
-    return "%s (%s)" % (self.value,
+    return "%s (%s)" % (self.val,
                         self.type)
 
   def __repr__(self):
     return repr(self.to_dict())
 
   def to_dict(self):
-    return {
-        # "start": self.start,
-        # "end": self.end,
-        # "row": self.row,
-        # "col": self.col,
+    d = {
         "pos": self.pos,
         "type": self.type,
-        "key": self.key,
-        "value": self.value
     }
+
+    if self.key is not None:
+      d["key"] = self.key
+
+    d["val"] = self.val
+    return d
