9f98381d020eeb548d0735d5c6fef342a8be5d91
Aamir Maniar <aamir.maniar@maniartech.com>
2020-06-12 13:32:43 +0530

    pretty print node

--- internet_object/parsers/nodes.py
+++ internet_object/parsers/nodes.py
@@ -1,18 +1,21 @@
 
 
-class Node:
+class Node(dict):
 
   def __init__(self, val, node_type, start, end, row, col, pos=0, parent=None):
-
+    self.type = node_type
+    self.pos = 0
     self.start = start
     self.end = end
     self.row = row
     self.col = col
 
-    self.pos = 0
     self.key = None
     self.val = val
-    self.type = node_type
+
+  def __setattr__(self, name, value):
+    dict.__setitem__(self, name, value)
+    return super().__setattr__(name, value)
 
   @classmethod
   def from_token(cls, token, pos=0, parent=None):
