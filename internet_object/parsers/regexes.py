7027c8920e0e97d5ac88a2d0905fc050018871e9
Aamir Maniar <aamir.maniar@maniartech.com>
2020-06-12 10:51:02 +0530

    lexer now properly identifies the numbers

--- internet_object/parsers/regexes.py
+++ internet_object/parsers/regexes.py
@@ -10,3 +10,5 @@ re_not_separator = re.compile(r'[^\{\}\[\]\:\,~]')
 re_not_regular_string = re.compile(r'[^\"]')
 
 re_regular_string = re.compile(r"^\"(?:[^\"\\]|\\.)*\"$")
+
+re_number = re.compile(r"^([-+]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?)$")
