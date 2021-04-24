9350f83197988a2869e9d4c9f5492d3391fe30c8
Aamir Maniar <aamir.maniar@maniartech.com>
2021-04-24 15:34:26 +0530

    Clean up!

--- internet_object/typedefs/__init__.py
+++ internet_object/typedefs/__init__.py
@@ -6,14 +6,8 @@ from .object import ObjectDef
 from .string import StringDef
 from .number import NumberDef
 
-# registry.register("any", AnyDef())
+registry.register("any", AnyDef())
 registry.register("object", ObjectDef())
 registry.register("string", StringDef("string"))
 registry.register("number", NumberDef("number"))
 registry.register("int", NumberDef("int"))
-
-
-schema_registry.register("object", ObjectDef())
-schema_registry.register("string", StringDef("string"))
-schema_registry.register("number", NumberDef("number"))
-schema_registry.register("int", NumberDef("int"))
